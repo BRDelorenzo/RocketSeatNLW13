1d870804041f8e7b137e211fa866585f593d998b
BRDelorenzo <giovanni.delorenzo1998@gmail.com>
2023-09-14 15:12:15 -0300

    criando front-end

--- upload-ai-web/tailwind.config.js
+++ upload-ai-web/tailwind.config.js
@@ -17,6 +17,17 @@ module.exports = {
     },
     extend: {
       colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'slate':'#64748b',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
