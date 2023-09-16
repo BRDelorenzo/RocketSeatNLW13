a0c0534d4d6864b674484451161fad1813dd6723
BRDelorenzo <giovanni.delorenzo1998@gmail.com>
2023-09-16 18:30:41 -0300

    finalizando front-end nlw13

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,9 @@
 import { Separator } from "./components/ui/separator";
 import { Button } from "./components/ui/button";
-import { Github, Video,Upload } from 'lucide-react'
+import { Github, Video,Upload, Wand2 } from 'lucide-react'
 import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
   return (
@@ -36,12 +38,12 @@ export function App() {
         <div className="flex flex-col flex-1">
           <div className="grid grid-rows-2 gap-4 flex-1">
             <textarea
-              className="resize-none p-5 leading-relaxed rounded-md bg-sky-950"
+              className="resize-none p-5 leading-relaxed rounded-md bg-sky-950/0 hover:bg-sky-950"
               placeholder="Add Prompt"
 
             />
             <textarea
-              className="resize-none p-5 leading-relaxed rounded-md bg-sky-950"
+              className="resize-none p-5 leading-relaxed rounded-md bg-sky-950/0 hover:bg-sky-950"
               placeholder="Result"
               readOnly />
           </div>
@@ -63,18 +65,74 @@ export function App() {
             <input type="file" id="video" accept="video/mp4" className="sr-only">
             </input>
             <Separator />
-            <div className="space-y-1">
+            <div className="space-y-2">
               <label htmlFor="transcription_prompt">Transcription Prompt</label>
         
               <textarea
                 id="transcription_prompt"
-                className="min-h-20 leading-relaxed w-80 space-y-6 rounded-md text-sm items-center text-left p-5 bg-sky-900/0 hover:bg-sky-950"
+                className="h-20 resize-none leading-relaxed w-80 space-y-6 rounded-md text-sm items-center text-left p-5 bg-sky-900/0 hover:bg-sky-950"
                 placeholder="add key-words related on video separated with comma( , )." />
             </div>
             <Button className="bg-sky-950/0 text-white w-full hover:bg-sky-950">
               Upload Video!
               <Upload className="w-4 h-4 ml-2 bg"/>
             </Button>
+          </form>
+
+          <Separator></Separator>
+          <form className="space-y-6">
+            <div className="space-y-2">
+            <Label>Prompt</Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select Prompt"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="title">Youtube Shots Title</SelectItem>
+                <SelectItem value="description">Description Shots Title</SelectItem>
+              </SelectContent>
+            </Select>
+            </div>
+
+            <div className="space-y-2">
+            <Label>Model</Label>
+            <Select disabled defaultValue="gpt3.5">
+              <SelectTrigger>
+                <SelectValue></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="gpt3.5">GPT 3.5-Turbo 16K</SelectItem>
+              </SelectContent>
+            </Select>
+            <div className="block text-xs text-muted-foreground italic">
+              You will be able to select GPT Model soon.
+            </div>
+            </div>
+            
+
+            <Separator></Separator>
+
+          <div className="space-y-4">
+
+          </div>
+            <Label>Temperature</Label>
+            <Slider
+            min={0}
+            max={1}
+            step={0.1}
+            className="to-blue-950"
+            />
+           
+          <div className="block text-xs text-muted-foreground italic leading-relaxed">
+              Highest Values maybe turn the results more creatives and with errors.
+          </div>
+
+          <Separator></Separator>
+          <Button type="submit" className="w-full bg-sky-950/0 hover:bg-sky-950 text-white">
+            Submit
+            <Wand2 className="ml-5"/>
+          </Button>
+
           </form>
         </aside>
       </main>
