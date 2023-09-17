c565c28a91aa3dae0bb855192ddc55703f947a8b
BRDelorenzo <giovanni.delorenzo1998@gmail.com>
2023-09-17 16:09:37 -0300

    resolvendo bug do back-end

--- upload-ai-server/src/routes/upload-video.ts
+++ upload-ai-server/src/routes/upload-video.ts
@@ -1,11 +1,11 @@
 import {FastifyInstance } from "fastify";
-import { prisma } from '../lib/prisma';
 import { fastifyMultipart } from '@fastify/multipart'
 import path from "node:path";
 import { randomUUID } from 'node:crypto'
 import fs from 'node:fs'
 import { pipeline } from 'node:stream'
 import { promisify } from "node:util";
+import { prisma } from '../lib/prisma';
 
 const pump = promisify(pipeline)
 
@@ -16,11 +16,12 @@ export async function uploadVideoRoute(app: FastifyInstance){
             fileSize: 1_048_576 * 25 //25mb
         }
     })
+
     app.post('/videos',async (request, reply)=>{
         const data = await request.file()
 
         if (!data){
-            return reply.send(400).send({error: 'Missing file input.'})
+            return reply.status(400).send({error: 'Missing file input.'})
         }
 
         const extension = path.extname(data.filename)
@@ -37,7 +38,16 @@ export async function uploadVideoRoute(app: FastifyInstance){
         
         await pump(data.file, fs.createWriteStream(uploadDestination))
 
-        return reply.send()
-
+        const video = await prisma.video.create({
+            data: {
+                name: data.filename,
+                path: uploadDestination
+            }
+            
+        })
+        
+        return {
+            video
+        }
     })
 }
